42d08bbdabaf3a71a75485f9f1d5924f55420dd6
Raihante <raehancyber@gmail.com>
2024-08-30 22:20:50 +0700

    main

---
+++ Fintopio.py
@@ -0,0 +1,201 @@
+# FINTOPIO BOT
+# Author    : @fakinsit
+# Date      : 30/08/24
+
+import os
+import time
+import sys
+import re
+import json
+import requests
+from urllib.parse import unquote
+from pyfiglet import Figlet
+from colorama import Fore
+from onlylog import Log
+
+api = "https://fintopio-tg.fintopio.com/api"
+header = {
+      "Accept": "application/json, text/plain, */*",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Referer": "https://fintopio-tg.fintopio.com/",
+      "Sec-Ch-Ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Microsoft Edge";v="128", "Microsoft Edge WebView2";v="128"',
+      "Sec-Ch-Ua-Mobile": "?0",
+      "Sec-Ch-Ua-Platform": "Windows",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+      "Webapp": "true"
+    }
+
+def banner():
+    os.system("title FINTOPIO BOT | t.me/fakinsit" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")
+    custom_fig = Figlet(font='slant')
+    print('')
+    print(custom_fig.renderText(' FINTOPIO'));
+    print(Fore.RED + '[#] [C] R E G E X    ' + Fore.GREEN + '[FINTOPIO BOT] $$ ' + Fore.RESET)
+    print(Fore.GREEN +'[#] Welcome & Enjoy !', Fore.RESET)
+    print(Fore.YELLOW +'[#] Having Troubles? PM Telegram [t.me/fakinsit] ', Fore.RESET)
+    print('')
+
+def runforeva(): 
+    with open('quentod.txt', 'r') as file:
+        queryh = file.read().splitlines()
+    try:
+            value = True
+            while (value):
+                for index, query_id in enumerate(queryh, start=1):
+                    getname(query_id)
+                    postrequest(getlogin(query_id))
+    except Exception as e:
+        Log.error('[MAIN] error :', e)
+        runforeva()
+
+def getlogin(querybro):
+    try:
+        url = api + "/auth/telegram?"
+        s = requests.Session()
+        s.headers.update({"Webapp": "true"})
+        response = s.get(url+querybro, headers=header)
+        jData=response.json()
+        jsontoken =  jData['token']
+        Log.success('Login success')
+        #Log.success(jsontoken)
+        return jsontoken
+    except:
+        Log.error('[getlogin] error')
+
+def getname(querybro):
+    try:
+        found = re.search('user=([^&]*)', querybro).group(1)
+        decodedUserPart = unquote(found)
+        userObj = json.loads(decodedUserPart)
+        Log.success('username : @' + userObj['username'])
+    except:
+        Log.error('[decodedUserPart] error')
+
+def checkin(tomket):
+    try:
+        url = api + "/daily-checkins"
+        s = requests.Session()
+        s.headers.update({"Authorization": "Bearer " + tomket})
+        response = s.get(url, headers=header)
+        jData=response.json()
+        Log.warn('reward daily : ' + str(jData['dailyReward']))
+        Log.warn('total login day :' + str(jData['totalDays']))
+        Log.success('daily reward claimed!')
+    except Exception as e:
+        Log.error('[checkin] failed :', e)
+
+def nuke(tomket, id, reward):
+    try:
+        url = api + "/clicker/diamond/complete"
+        s = requests.Session()
+        s.headers.update({"Authorization": "Bearer " + tomket, "Webapp": "true"})
+        textpayload = {"diamondNumber":id}
+        print(textpayload)
+        response = s.post(url, headers=header, json=textpayload)
+        if response.status_code != 200:
+            Log.error('[asteroid] failed to claim')
+        Log.success('asteroid was crushed!')
+        Log.warn('reward : ' + reward)
+    except Exception as e:
+        Log.error('[asteroid] failed to start :', e)
+
+def tanamtanamubi(tomket):
+    try:
+        url = api + "/farming/farm"
+        s = requests.Session()
+        s.headers.update({"Authorization": "Bearer " + tomket, "Webapp": "true"})
+        response = s.post(url, headers=header)
+        if response.status_code != 200:
+            Log.error('[asteroid] failed to claim')
+        Log.success('farming started!')
+    except:
+        Log.error('[farming] failed to start')
+
+def panenbrow(tomket):
+    try:
+        url = api + "/farming/claim"
+        s = requests.Session()
+        s.headers.update({"Authorization": "Bearer " + tomket, "Webapp": "true"})
+        are = s.post(url, headers=header)
+        Log.success('farming claimed!')
+    except:
+        Log.error('[farming] failed to claim')
+
+
+def postrequest(bearer):
+    urldata = '/referrals/data'
+    urldiamondstate = '/clicker/diamond/state'
+    urlfarmstate = '/farming/state'
+
+
+    s = requests.Session()
+    s.headers.update({"Authorization": "Bearer " + bearer})
+
+
+    try:
+        r = s.get(api+urldata, headers=header)
+        jData=r.json()
+        jsondaily =  jData['isDailyRewardClaimed']
+        if jsondaily == False:
+            Log.warn('daily reward not claimed yet')
+            Log.warn('claiming..')
+            checkin(bearer)
+        Log.warn('balance : ' + jData['balance'])
+
+    except Exception as e:
+        Log.error('[daily] error :', e)
+        time.sleep(5)
+        runforeva()
+
+    try:
+        r2 = s.get(api+urldiamondstate, headers=header)
+        jData=r2.json()
+        jsonsettings =  jData['settings']
+        jsonstate =  jData['state']
+        jsondiamondid =  jData['diamondNumber']
+        jsontotalreward =  jsonsettings['totalReward']
+        if jsonstate == 'available':
+            nuke(bearer, jsondiamondid, jsontotalreward)
+        elif jsonstate == 'unavailable':
+            Log.warn('asteroid unavailable yet!')
+        else:
+            Log.warn('asteroid crushed! waiting next round..')
+
+    except Exception as e:
+        Log.error('[asteroid] error :', e)
+        time.sleep(5)
+        runforeva()
+
+    try:
+        r3 = s.get(api+urlfarmstate, headers=header)
+        jData=r3.json()
+        jsonstate =  jData['state']
+        if jsonstate == 'idling':
+            tanamtanamubi(bearer)
+        elif jsonstate == 'farming':
+            Log.warn('farming not finished yet!')
+        elif jsonstate == 'farmed':
+            panenbrow(bearer)
+        else:
+            Log.error('[farming] error ')
+        print('=========================================')
+        Log.countdown(30)
+
+    except Exception as e:
+        Log.error('[farming] error :', e)
+        time.sleep(5)
+        runforeva()
+
+
+
+# NYALAIN SENDIRI ABANGKUHH
+if __name__ == "__main__":
+    try:
+        banner()
+        runforeva()
+    except KeyboardInterrupt:
+        sys.exit()
+
+
---
+++ README.md
@@ -0,0 +1,68 @@
+
+# 💲 FINTOPIO-HHH ![NOTICE](https://img.shields.io/badge/DO%20WITH%20YOUR%20OWN%20RISK%20%7C%20CEK%20SECARA%20BERKALA%20-f54842)
+
+![Logo](https://raw.githubusercontent.com/raihante/fintopioh/main/joinus.gif)
+
+
+## ✅ Features
+
+- [x]  Claim Daily Login
+- [x]  Monitor Account Info
+- [x]  Support Multiple Account
+- [x]  Auto Click Asteroid
+- [x]  Auto Farm
+
+
+## 🚀 Authors
+
+- [@fakinsit | R E G E X](https://www.t.me/fakinsit)
+
+
+## ⚙️ Installation
+Pertama, ambil query/user id mu
+
+```bash
+  Buka game, sebelum masuk ke game klik kanan inspect -> application -> local storage -> tgwebapp -> query_id
+```
+
+Buka quentod.txt paste query/user id telegram panjenengan sedoyo
+
+```bash
+  contoh query_id : query_id=xx&user=xx&auth_date=xx&hash=xx
+  contoh user_id : user_id=xx&user=xx&auth_date=xx&hash=xx
+```
+
+Pastikan 'python' sudah ter-install di pc / hp kalian, buka terminal atau command prompt
+
+```bash
+  python -m pip install -r requirements.txt
+```
+    
+## ▶️ Running
+Jika query/user id dan requirements sudah diisi dan terinstall maka selanjutnya ketik command di bawah ini
+
+```bash
+  python Fintopio.py
+```
+
+Preview running:
+![Run](https://raw.githubusercontent.com/raihante/fintopioh/main/run.png)
+
+
+## ❓ FAQ
+
+#### Bang kok "ERROR RESTARTING BOT" mulu ?
+
+Coba ambil query (token) nya lagi kemungkinan expired.
+
+#### [?] Bang link daftar / register botnya dimana ?
+[![Register BOT](https://img.shields.io/badge/REGISTER-HERE-yellow.svg)](https://t.me/hopium_official_bot/join?startapp=linkCode_768014636)
+
+
+## License
+
+
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+
